50b5334e0538d4db716d35d39ab394fd27421f21
priyam4501 <priyamsingh4501@gmail.com>
2026-05-26 02:31:24 +0530

    feat(discovery-server): setup Eureka server

---
+++ discovery-server/src/test/java/com/pulsewave/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pulsewave.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
